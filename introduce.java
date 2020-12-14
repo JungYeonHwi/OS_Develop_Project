24a7aff48d8d4500f55d712decd04ea7d399839c
JungYeonHwi <wjddusgnl676@naver.com>
2020-12-15 04:34:30 +0900

    introduce

---
+++ introduce.java
@@ -0,0 +1,25 @@
+/*****************************
+
+* 프로그램명: introduce_example
+
+* 작성자: 정연휘
+
+* 이름: 정연휘
+
+* 작성일 : 2020.09.13
+
+*프로그램 설명 : 자기소개 프로그램
+
+*********************************/
+
+package introduce_example;
+
+public class Hello2030 {
+	public static void main(String[] args) {
+		System.out.print("학과 : 소프트웨어학과\n");
+		System.out.print("학년 : 3학년\n");
+		System.out.print("입학년도 : 2018년\n");
+		System.out.print("이름 : 정연휘\n");
+	}
+}
+}
